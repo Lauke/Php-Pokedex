79094c3fc454408dbd08708fea9939b1debab563
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-19 19:28:24 +0200

    added search function, basic html

--- index-OOP.php
+++ index-OOP.php
@@ -1,6 +1,7 @@
 <?php
 
-class Pokemon{
+class Pokemon
+{
     //Properties
     private $name;
     private $id;
@@ -11,48 +12,59 @@ class Pokemon{
     private $move4;
 
     //Constructor
-    public function __construct($name,$id,$image,$move1,$move2,$move3,$move4){
-        $this->name=$name;
-        $this->id=$id;
-        $this->image=$image;
-        $this->move1=$move1;
-        $this->move2=$move2;
-        $this->move3=$move3;
-        $this->move4=$move4;
+    public function __construct($name, $id, $image, $move1, $move2, $move3, $move4)
+    {
+        $this->name = $name;
+        $this->id = $id;
+        $this->image = $image;
+        $this->move1 = $move1;
+        $this->move2 = $move2;
+        $this->move3 = $move3;
+        $this->move4 = $move4;
     }
 
     //Methods
-    public function getName(){
+    public function getName()
+    {
         return $this->name;
     }
 
-    public function getId(){
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getImage(){
+    public function getImage()
+    {
         return $this->image;
     }
 
-    public function getMove1(){
+    public function getMove1()
+    {
         return $this->move1;
     }
 
-    public function getMove2(){
+    public function getMove2()
+    {
         return $this->move2;
     }
 
-    public function getMove3(){
+    public function getMove3()
+    {
         return $this->move3;
     }
 
-    public function getMove4(){
+    public function getMove4()
+    {
         return $this->move4;
     }
 }
 
 // ADDED A CHANGE TO LOWER CASE FUNCTION TO AVOID ERRORS WHEN USING CAPS
-$api_url = 'https://pokeapi.co/api/v2/pokemon/1';
+if (!empty($_GET['search'])) {
+
+$_GET['search'] = strtolower($_GET['search']);
+$api_url = 'https://pokeapi.co/api/v2/pokemon/' . $_GET['search'];
 
 // Read JSON file
 $json_data = file_get_contents($api_url);
@@ -77,12 +89,43 @@ $pokemonOne = new Pokemon(
     $pokemon_data['moves']['3']['move']['name']
 );
 
-echo $pokemonOne->getName();
-echo $pokemonOne->getId();
-echo $pokemonOne->getImage();
-echo $pokemonOne->getMove1();
-echo $pokemonOne->getMove2();
-echo $pokemonOne->getMove3();
-echo $pokemonOne->getMove4();
+} else {
+    
+}
+
 
 ?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pokedex</title>
+</head>
+
+<body>
+
+    <form action="">
+        <input type="text" name="search" class="rounded-pill form-input">
+        <button type="submit" class="btn rounded-pill btn-searchpoke">Search pokemon</button>
+    </form>
+
+    <?php
+    if (!empty($_GET['search'])) :
+    ?>
+
+<?php echo $pokemonOne->getName() . '<br>'; ?>
+<?php echo $pokemonOne->getId() . '<br>'; ?>
+<?php echo $pokemonOne->getImage() . '<br>'; ?>
+<?php echo $pokemonOne->getMove1() . '<br>'; ?>
+<?php echo $pokemonOne->getMove2() . '<br>'; ?>
+<?php echo $pokemonOne->getMove3() . '<br>'; ?>
+<?php echo $pokemonOne->getMove4() . '<br>'; ?>
+
+    <?php else :
+        echo '<div class="roundy">Search a pokemon by name or by ID</div>';
+    endif; ?>
+
+</body>
+
+</html>
